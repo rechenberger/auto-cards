1ff23aa358f9093cc4dc60b85ceb90079b850514
Tristan Rechenberger <rechenberger@me.com>
2024-04-14 15:56:05 +0200

    ✨ magic link auth

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,9 +2,16 @@ import { db } from '@/db/db'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import NextAuth from 'next-auth'
 import Discord from 'next-auth/providers/discord'
+import Resend from 'next-auth/providers/resend'
 import { CredentialsProvider } from './CredentialsProvider'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Discord, CredentialsProvider],
+  providers: [
+    Discord,
+    Resend({
+      from: process.env.EMAIL_FROM,
+    }),
+    CredentialsProvider,
+  ],
 })
