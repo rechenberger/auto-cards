ca9e74fa806f161b14e616bcd98bbf3046299ac3
Tristan Rechenberger <rechenberger@me.com>
2024-04-14 12:01:25 +0200

    🎉

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,18 +4,26 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 export default async function Page() {
-  const p = path.join(process.cwd(), '/README.md')
-  const readme = await fs.readFile(p, 'utf8')
   return (
     <>
       <div className="flex-1 flex flex-col items-center justify-center gap-12 py-8">
         <h1 className="text-2xl lg:text-6xl">🎉 Welcome to the Party 🥳</h1>
-        <Card className="max-w-full py-8">
-          <CardContent>
-            <Markdown className="break-words">{readme}</Markdown>
-          </CardContent>
-        </Card>
+        <Readme />
       </div>
+    </>
+  )
+}
+
+const Readme = async () => {
+  const p = path.join(process.cwd(), '/README.md')
+  const readme = await fs.readFile(p, 'utf8')
+  return (
+    <>
+      <Card className="max-w-full py-8">
+        <CardContent>
+          <Markdown className="break-words">{readme}</Markdown>
+        </CardContent>
+      </Card>
     </>
   )
 }
