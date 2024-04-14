cf47de4dbd6e386d77dbd52f984aa531195c807f
Tristan Rechenberger <rechenberger@me.com>
2024-04-14 11:32:35 +0200

    🎉

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from '@/auth/UserButton'
+
 export default function Page() {
   return (
     <>
-      <h1 className="text-3xl">Home</h1>
+      <div className="flex-1 flex flex-col items-center justify-center gap-4 pb-80">
+        <h1 className="text-2xl lg:text-6xl">🎉 Welcome to the Party 🥳</h1>
+        <UserButton />
+      </div>
     </>
   )
 }
