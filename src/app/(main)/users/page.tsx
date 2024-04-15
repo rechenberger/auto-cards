cfafe0acffc51d3d5ddc82b2baf8dfeecc7e2324
Tristan Rechenberger <rechenberger@me.com>
2024-04-15 22:59:21 +0200

    ✨ auth

--- src/app/(main)/users/page.tsx
+++ src/app/(main)/users/page.tsx
@@ -1,5 +1,12 @@
 import { notFoundIfNotAdmin } from '@/auth/getIsAdmin'
-import { SimpleDataCard } from '@/components/simple/SimpleDataCard'
+import { LocalDateTime } from '@/components/demo/LocalDateTime'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { db } from '@/db/db'
 import { users as usersTable } from '@/db/schema-auth'
 import {
@@ -8,7 +15,6 @@ import {
 } from '@/super-action/action/createSuperAction'
 import { ActionButton } from '@/super-action/button/ActionButton'
 import { eq } from 'drizzle-orm'
-import { omit } from 'lodash-es'
 import { Metadata } from 'next'
 import { revalidatePath } from 'next/cache'
 import { Fragment } from 'react'
@@ -23,41 +29,66 @@ export default async function Page() {
   const users = await db.query.users.findMany({})
   return (
     <>
-      <CreateUserButton />
-      <div className="grid grid-cols-[1fr_auto] gap-4">
+      <div className="flex flex-row gap-2 items-center">
+        <CardTitle className="flex-1">Users</CardTitle>
+        <CreateUserButton />
+      </div>
+      <div className="grid lg:grid-cols-3 gap-4">
         {users.map((user) => {
           const isAdmin = !!user.isAdmin
           return (
             <Fragment key={user.id}>
-              <SimpleDataCard data={omit(user, ['passwordHash'])} />
-              <div>
-                <ActionButton
-                  variant={isAdmin ? 'destructive' : 'default'}
-                  action={async () => {
-                    'use server'
-                    return superAction(async () => {
-                      await db
-                        .update(usersTable)
-                        .set({ isAdmin: !isAdmin })
-                        .where(eq(usersTable.id, user.id))
-                      streamToast({
-                        title: isAdmin ? 'Removed admin' : 'Made admin',
-                        description: `User ${user.email} is now ${
-                          isAdmin ? 'not' : ''
-                        } an admin`,
-                      })
-                      revalidatePath('/users')
-                    })
-                  }}
-                  command={{
-                    label: `${isAdmin ? 'Remove' : 'Make'} admin: ${
-                      user.email
-                    }`,
-                  }}
-                >
-                  {isAdmin ? 'Remove admin' : 'Make admin'}
-                </ActionButton>
-              </div>
+              <Card>
+                <CardHeader>
+                  <CardTitle>{user.name ?? user.email}</CardTitle>
+                  <CardDescription>{user.id}</CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                  <div>
+                    <div>{user.email}</div>
+                    <div className="text-muted-foreground">
+                      {user.emailVerified ? (
+                        <>
+                          Verified{' '}
+                          <LocalDateTime
+                            datetime={user.emailVerified.toISOString()}
+                          />
+                        </>
+                      ) : (
+                        <>Not verified</>
+                      )}
+                    </div>
+                  </div>
+                  <div className="">
+                    <ActionButton
+                      variant={isAdmin ? 'destructive' : 'default'}
+                      action={async () => {
+                        'use server'
+                        return superAction(async () => {
+                          await db
+                            .update(usersTable)
+                            .set({ isAdmin: !isAdmin })
+                            .where(eq(usersTable.id, user.id))
+                          streamToast({
+                            title: isAdmin ? 'Removed admin' : 'Made admin',
+                            description: `User ${user.email} is now ${
+                              isAdmin ? 'not' : ''
+                            } an admin`,
+                          })
+                          revalidatePath('/users')
+                        })
+                      }}
+                      command={{
+                        label: `${isAdmin ? 'Remove' : 'Make'} admin: ${
+                          user.email
+                        }`,
+                      }}
+                    >
+                      {isAdmin ? 'Remove admin' : 'Make admin'}
+                    </ActionButton>
+                  </div>
+                </CardContent>
+              </Card>
             </Fragment>
           )
         })}
