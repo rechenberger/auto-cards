8685a7eae2bcd74d55a4439f54fc8575f3327a95
Tristan Rechenberger <rechenberger@me.com>
2024-08-13 19:39:44 +0200

    🎉 game

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,23 @@
 import { createId } from '@paralleldrive/cuid2'
 import { relations } from 'drizzle-orm'
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { users } from './schema-auth'
 
 export * from './schema-auth'
 
-export const games = sqliteTable('game', {
+const baseStats = () => ({
   id: text('id')
     .notNull()
     .primaryKey()
     .$defaultFn(() => createId()),
-  userId: text('userId').notNull(),
-  data: text('metadata', { mode: 'json' }).$type<any>().notNull(),
   createdAt: text('createdAt'),
   updatedAt: text('updatedAt'),
+})
+
+export const games = sqliteTable('game', {
+  ...baseStats(),
+  userId: text('userId').notNull(),
+  data: text('data', { mode: 'json' }).$type<object>().notNull(),
 })
 
 export const gameRelations = relations(games, ({ one, many }) => ({
@@ -21,4 +25,71 @@ export const gameRelations = relations(games, ({ one, many }) => ({
     fields: [games.userId],
     references: [users.id],
   }),
+  loadouts: many(loadout),
+  matches: many(match),
 }))
+
+export const loadout = sqliteTable('loadout', {
+  ...baseStats(),
+  userId: text('userId').notNull(),
+  data: text('data', { mode: 'json' }).$type<object>().notNull(),
+  gameId: text('gameId').notNull(),
+  roundNo: int('roundNo').notNull(),
+  primaryMatchParticipationId: text('primaryMatchParticipationId'),
+})
+
+export const loadoutRelations = relations(loadout, ({ one, many }) => ({
+  user: one(users, {
+    fields: [loadout.userId],
+    references: [users.id],
+  }),
+  game: one(games, {
+    fields: [loadout.gameId],
+    references: [games.id],
+  }),
+  matchParticipations: many(matchParticipation),
+  primaryMatchParticipation: one(matchParticipation, {
+    fields: [loadout.primaryMatchParticipationId],
+    references: [matchParticipation.id],
+  }),
+}))
+
+export const match = sqliteTable('match', {
+  ...baseStats(),
+  data: text('data', { mode: 'json' }).$type<object>().notNull(),
+})
+
+export const matchRelations = relations(match, ({ one, many }) => ({
+  game: one(games, {
+    fields: [match.id],
+    references: [games.id],
+  }),
+  matchParticipations: many(matchParticipation),
+}))
+
+export const matchParticipation = sqliteTable('matchParticipation', {
+  ...baseStats(),
+  data: text('data', { mode: 'json' }).$type<object>().notNull(),
+  matchId: text('matchId').notNull(),
+  userId: text('userId').notNull(),
+  loadoutId: text('loadoutId').notNull(),
+  sideIdx: int('sideIdx').notNull(),
+})
+
+export const matchParticipationRelations = relations(
+  matchParticipation,
+  ({ one, many }) => ({
+    match: one(match, {
+      fields: [matchParticipation.matchId],
+      references: [match.id],
+    }),
+    user: one(users, {
+      fields: [matchParticipation.userId],
+      references: [users.id],
+    }),
+    loadout: one(loadout, {
+      fields: [matchParticipation.loadoutId],
+      references: [loadout.id],
+    }),
+  }),
+)
