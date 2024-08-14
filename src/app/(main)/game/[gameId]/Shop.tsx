541abeab3de6faad8aed2e7568a2eedba1262211
Tristan Rechenberger <rechenberger@me.com>
2024-08-14 09:13:28 +0200

    🎉 game

--- src/app/(main)/game/[gameId]/Shop.tsx
+++ src/app/(main)/game/[gameId]/Shop.tsx
@@ -1,9 +1,7 @@
 import { Game } from '@/db/schema-zod'
+import { gameAction } from '@/game/gameAction'
 import { generateShopItems } from '@/game/generateShopItems'
-import { updateGame } from '@/game/updateGame'
-import { superAction } from '@/super-action/action/createSuperAction'
 import { ActionButton } from '@/super-action/button/ActionButton'
-import { revalidatePath } from 'next/cache'
 import { Fragment } from 'react'
 import { ItemCard } from './ItemCard'
 
@@ -25,33 +23,34 @@ export const Shop = ({ game }: { game: Game }) => {
       <div className="flex flex-row gap-2 justify-center items-center">
         <div>${game.data.gold}</div>
         <ActionButton
+          catchToast
           action={async () => {
             'use server'
-            return superAction(async () => {
-              game.data.gold += 10
-              await updateGame({
-                game,
-              })
-              revalidatePath('/', 'layout')
+            return gameAction({
+              gameId: game.id,
+              action: async ({ ctx }) => {
+                ctx.game.data.gold += 10
+              },
             })
           }}
         >
           +${10}
         </ActionButton>
         <ActionButton
+          catchToast
           action={async () => {
             'use server'
-            return superAction(async () => {
-              game.data.shopRerolls += 1
-              if (game.data.gold < priceToReroll) {
-                throw new Error('not enough gold')
-              }
-              game.data.gold -= priceToReroll
-              game.data.shopItems = await generateShopItems({ game })
-              await updateGame({
-                game,
-              })
-              revalidatePath('/', 'layout')
+            return gameAction({
+              gameId: game.id,
+              action: async ({ ctx }) => {
+                const { game } = ctx
+                game.data.shopRerolls += 1
+                if (game.data.gold < priceToReroll) {
+                  throw new Error('not enough gold')
+                }
+                game.data.gold -= priceToReroll
+                game.data.shopItems = await generateShopItems({ game })
+              },
             })
           }}
         >
