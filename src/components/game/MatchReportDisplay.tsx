2fd54b14fd3d8aa8f452a97c7fd5a015576e51fc
Tristan Rechenberger <rechenberger@me.com>
2024-08-14 20:16:50 +0200

    ✨ generateMatch

--- src/components/game/MatchReportDisplay.tsx
+++ src/components/game/MatchReportDisplay.tsx
@@ -1,7 +1,8 @@
 import { MatchReport } from '@/game/generateMatch'
 import { cn } from '@/lib/utils'
 import { capitalCase } from 'change-case'
-import { ArrowRight } from 'lucide-react'
+import { keys, pick, pickBy } from 'lodash-es'
+import { ArrowRight, Swords } from 'lucide-react'
 import { Fragment } from 'react'
 import { StatsDisplay } from './StatsDisplay'
 
@@ -12,21 +13,30 @@ export const MatchReportDisplay = ({
 }) => {
   return (
     <>
-      <div className="grid grid-cols-[auto,auto,auto,1fr,auto] text-xs rounded-md overflow-hidden">
+      <div className="grid grid-cols-[auto,auto,auto,1fr,auto,auto] text-xs rounded-md overflow-hidden">
         {matchReport.logs.map((log, idx) => {
           const cell = cn(
             'px-2 py-0.5 flex flex-row items-center h-6',
             log.sideIdx === 0 ? 'bg-blue-500/20' : 'bg-red-500/20',
           )
           const hasStats = !!log.stats
+
+          const statsAfter =
+            log.targetSideIdx !== undefined &&
+            log.stats &&
+            pick(
+              log.stateSnapshot.sides[log.targetSideIdx].stats,
+              keys(pickBy(log.stats, (v) => !!v)),
+            )
+
           return (
             <Fragment key={idx}>
               <div className={cn(cell)}>{(log.time / 1000).toFixed(3)}</div>
               <div className={cn(cell, 'flex flex-row gap 1 items-center')}>
-                {log.targetSideIdx === undefined ? null : log.sideIdx ===
-                  log.targetSideIdx ? null : (
-                  <ArrowRight className="size-3" />
-                )}
+                {log.targetSideIdx !== undefined &&
+                  log.sideIdx !== log.targetSideIdx && (
+                    <Swords className="size-3" />
+                  )}
               </div>
               {hasStats && (
                 <div className={cn(cell)}>
@@ -38,6 +48,14 @@ export const MatchReportDisplay = ({
               </div>
               <div className={cn(cell)}>
                 {log.itemName && <>with {capitalCase(log.itemName)}</>}
+              </div>
+              <div className={cn(cell, 'flex flex-row gap-1')}>
+                {statsAfter && (
+                  <>
+                    <ArrowRight className="size-3" />
+                    <StatsDisplay stats={statsAfter} size="sm" />
+                  </>
+                )}
               </div>
             </Fragment>
           )
