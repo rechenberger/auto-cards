d05ae605351d6492aceea8b0b630ab982e6f135a
Tristan Rechenberger <rechenberger@me.com>
2024-08-18 15:50:49 +0200

    ✨ bot

--- src/app/(main)/bot/page.tsx
+++ src/app/(main)/bot/page.tsx
@@ -1,39 +1,116 @@
 import { throwIfNotAdmin } from '@/auth/getIsAdmin'
-import { superAction } from '@/super-action/action/createSuperAction'
+import { SimpleDataCard } from '@/components/simple/SimpleDataCard'
+import { Card } from '@/components/ui/card'
+import { db } from '@/db/db'
+import { schema } from '@/db/schema-export'
+import { Loadout } from '@/db/schema-zod'
+import { roundStats } from '@/game/roundStats'
+import {
+  streamDialog,
+  superAction,
+} from '@/super-action/action/createSuperAction'
 import { ActionButton } from '@/super-action/button/ActionButton'
+import { isNull } from 'drizzle-orm'
+import { sumBy } from 'lodash-es'
 import { Metadata } from 'next'
-import { SimulationInput } from '../simulation/simulate'
+import { Fragment } from 'react'
+import { simulate, SimulationInput } from '../simulation/simulate'
+import { SimulationDisplay } from '../simulation/SimulationDisplay'
 
 export const metadata: Metadata = {
   title: 'Bot',
 }
 
 const baseInput: SimulationInput = {
-  noOfBots: 40,
+  noOfBots: 10,
   noOfRepeats: 1,
-  simulationSeed: ['lol'],
-  startingGold: 40,
+  simulationSeed: ['bot'],
+  startingGold: 0,
   startingItems: ['hero'],
-  noOfBotsSelected: 20,
+  noOfBotsSelected: 5,
   noOfSelectionRounds: 5,
 }
 
 export default async function Page() {
   await throwIfNotAdmin({ allowDev: true })
-  const userId = process.env.BOT_USER_ID
-  if (!userId) throw new Error('BOT_USER_ID not set')
+  // const userId = process.env.BOT_USER_ID
+  // if (!userId) throw new Error('BOT_USER_ID not set')
+
+  const allLoadouts = await db.query.loadout
+    .findMany({
+      where: isNull(schema.loadout.userId),
+    })
+    .then(Loadout.array().parse)
+
+  const rounds = roundStats.map(({ roundNo }) => {
+    const loadouts = allLoadouts.filter((r) => r.roundNo === roundNo)
+    return {
+      roundNo,
+      loadouts,
+      gold: startingGoldByRound(roundNo),
+    }
+  })
+
+  const generateRoundBots = async ({ roundNo }: { roundNo: number }) => {
+    'use server'
+    return superAction(async () => {
+      const input = {
+        ...baseInput,
+        startingGold: startingGoldByRound(roundNo),
+      }
+      const simulationResult = await simulate({
+        input,
+      })
+      if (!simulationResult) return
+      streamDialog({
+        title: `Round ${roundNo}`,
+        content: (
+          <div>
+            <SimulationDisplay
+              input={input}
+              simulationResult={simulationResult}
+              allItems={[]}
+            />
+          </div>
+        ),
+      })
+    })
+  }
+
   return (
     <>
+      {/* <ActionButton
+        action={async () => {
+          'use server'
+          return superAction(async () => {})
+        }}
+      >
+        BOT
+      </ActionButton> */}
       <div className="self-center flex flex-col gap-4">
-        <ActionButton
-          action={async () => {
-            'use server'
-            return superAction(async () => {})
-          }}
-        >
-          BOT
-        </ActionButton>
+        {rounds.map((round) => (
+          <Fragment key={round.roundNo}>
+            <Card className="p-2">
+              <SimpleDataCard data={round} />
+              <ActionButton
+                action={async () => {
+                  'use server'
+                  return generateRoundBots({ roundNo: round.roundNo })
+                }}
+              >
+                Generate
+              </ActionButton>
+            </Card>
+          </Fragment>
+        ))}
       </div>
     </>
+  )
+}
+
+const startingGoldByRound = (roundNo: number) => {
+  return sumBy(
+    roundStats.filter((r) => r.roundNo <= roundNo),
+    'gold',
   )
 }
--- src/game/roundStats.ts
+++ src/game/roundStats.ts
@@ -1,94 +1,94 @@
 import { Stats } from './stats'
-type RoundStats = Stats & { round: number }
+type RoundStats = Stats & { roundNo: number }
 
 export const roundStats: RoundStats[] = [
   {
-    round: 0,
+    roundNo: 0,
     gold: 12,
     health: 50,
   },
   {
-    round: 1,
+    roundNo: 1,
     gold: 9,
     health: 70,
   },
   {
-    round: 2,
+    roundNo: 2,
     gold: 9,
     health: 90,
   },
   {
-    round: 3,
+    roundNo: 3,
     gold: 9,
     health: 110,
   },
   {
-    round: 4,
+    roundNo: 4,
     gold: 10,
     health: 140,
   },
   {
-    round: 5,
+    roundNo: 5,
     gold: 10,
     health: 170,
   },
   {
-    round: 6,
+    roundNo: 6,
     gold: 11,
     health: 200,
   },
   {
-    round: 7,
+    roundNo: 7,
     gold: 11, // Sub-class thingy gives another 10 gold
     health: 230,
   },
   {
-    round: 8,
+    roundNo: 8,
     gold: 12,
     health: 260,
   },
   {
-    round: 9,
+    roundNo: 9,
     gold: 12,
     health: 300,
   },
   {
-    round: 10,
+    roundNo: 10,
     gold: 13,
     health: 340,
   },
   {
-    round: 11,
+    roundNo: 11,
     gold: 13,
     health: 380,
   },
   {
-    round: 12,
+    roundNo: 12,
     gold: 14,
     health: 420,
   },
   {
-    round: 13,
+    roundNo: 13,
     gold: 14,
     health: 460,
   },
   {
-    round: 14,
+    roundNo: 14,
     gold: 15,
     health: 520,
   },
   {
-    round: 15,
+    roundNo: 15,
     gold: 15,
     health: 580,
   },
   {
-    round: 16,
+    roundNo: 16,
     gold: 15,
     health: 640,
   },
   {
-    round: 17,
+    roundNo: 17,
     gold: 15,
     health: 700,
   },
