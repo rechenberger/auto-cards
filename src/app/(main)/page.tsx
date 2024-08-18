d33de905000695ca7b8b5c28201ea9530f80c823
Tristan Rechenberger <rechenberger@me.com>
2024-08-18 10:05:34 +0200

    💄 TitleScreen

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,29 @@
 import { getIsLoggedIn } from '@/auth/getMyUser'
-import { UserButton } from '@/auth/UserButton'
-import { cn } from '@/lib/utils'
-import { redirect } from 'next/navigation'
+import dynamic from 'next/dynamic'
+
+const TitleScreen = dynamic(
+  () => import('@/components/game/TitleScreen').then((m) => m.TitleScreen),
+  {
+    ssr: false,
+  },
+)
 
 export default async function Page() {
   const isLoggedIn = await getIsLoggedIn()
-  if (isLoggedIn) {
-    redirect('/game')
-  }
+  // if (isLoggedIn) {
+  //   redirect('/game')
+  // }
 
   return (
     <>
-      <div className="flex-1 flex flex-col items-center justify-center gap-4 py-8 mb-80">
+      {/* <div className="flex-1 flex flex-col items-center justify-center gap-4 py-8 mb-80">
         <h1 className={cn('font-bold text-2xl lg:text-6xl')}>
           Auto <span className="text-primary">Cards</span>
         </h1>
         <UserButton />
+      </div> */}
+      <div className=" flex-1 h-screen">
+        <TitleScreen />
       </div>
     </>
   )
