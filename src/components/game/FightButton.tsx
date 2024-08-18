f12eb9b2cd88d0b0dba3a116e2cc1998663eee41
Tristan Rechenberger <rechenberger@me.com>
2024-08-18 18:39:42 +0200

    💄

--- src/components/game/FightButton.tsx
+++ src/components/game/FightButton.tsx
@@ -1,9 +1,7 @@
 import { Game } from '@/db/schema-zod'
 import { fight } from '@/game/fight'
 import { gameAction } from '@/game/gameAction'
-import { streamDialog } from '@/super-action/action/createSuperAction'
 import { ActionButton } from '@/super-action/button/ActionButton'
-import { MatchReportDisplay } from './MatchReportDisplay'
 
 export const FightButton = ({ game }: { game: Game }) => {
   return (
@@ -16,18 +14,18 @@ export const FightButton = ({ game }: { game: Game }) => {
             gameId: game.id,
             action: async ({ ctx }) => {
               const { game } = ctx
-              const { matchReport, enemyLoadout } = await fight({ game })
-              streamDialog({
-                title: 'Match Report',
-                content: (
-                  <>
-                    {/* <LoadoutDisplay loadout={enemyLoadout.data} /> */}
-                    <div className="overflow-auto max-h-[calc(100vh-240px)] max-sm:-mx-6">
-                      <MatchReportDisplay matchReport={matchReport} />
-                    </div>
-                  </>
-                ),
-              })
+              const { matchReport } = await fight({ game })
+              // streamDialog({
+              //   title: 'Match Report',
+              //   content: (
+              //     <>
+              //       {/* <LoadoutDisplay loadout={enemyLoadout.data} /> */}
+              //       <div className="overflow-auto max-h-[calc(100vh-240px)] max-sm:-mx-6">
+              //         <MatchReportDisplay matchReport={matchReport} />
+              //       </div>
+              //     </>
+              //   ),
+              // })
             },
           })
         }}
--- src/components/game/MatchView.tsx
+++ src/components/game/MatchView.tsx
@@ -3,6 +3,7 @@ import { schema } from '@/db/schema-export'
 import { Game, Match } from '@/db/schema-zod'
 import { generateMatch } from '@/game/generateMatch'
 import { eq } from 'drizzle-orm'
+import { orderBy } from 'lodash-es'
 import { LoadoutDisplay } from './LoadoutDisplay'
 import { MatchReportDisplay } from './MatchReportDisplay'
 import { NextRoundButton } from './NextRoundButton'
@@ -14,12 +15,13 @@ export const MatchView = async ({
   game?: Game
   match: Match
 }) => {
-  const participants = await db.query.matchParticipation.findMany({
+  let participants = await db.query.matchParticipation.findMany({
     where: eq(schema.matchParticipation.matchId, match.id),
     with: {
       loadout: true,
     },
   })
+  participants = orderBy(participants, (p) => p.sideIdx, 'asc')
 
   const matchReport = await generateMatch({
     participants: participants.map((p) => ({ loadout: p.loadout.data })),
@@ -29,12 +31,12 @@ export const MatchView = async ({
   return (
     <>
       <div className="rotate-180">
-        <LoadoutDisplay loadout={participants[0].loadout.data} />
+        <LoadoutDisplay loadout={participants[1].loadout.data} />
       </div>
       <div className="max-h-96 overflow-scroll">
         <MatchReportDisplay matchReport={matchReport} />
       </div>
-      <LoadoutDisplay loadout={participants[1].loadout.data} />
+      <LoadoutDisplay loadout={participants[0].loadout.data} />
       {!!game && <NextRoundButton game={game} />}
     </>
   )
