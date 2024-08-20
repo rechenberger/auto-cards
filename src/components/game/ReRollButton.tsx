f0fff795ef3f26cd84e7795be08c6900705c8d91
Tristan Rechenberger <rechenberger@me.com>
2024-08-20 10:19:45 +0200

    💄 ReRollButton

--- src/components/game/ReRollButton.tsx
+++ src/components/game/ReRollButton.tsx
@@ -4,6 +4,7 @@ import { generateShopItems } from '@/game/generateShopItems'
 import { cn } from '@/lib/utils'
 import { ActionButton } from '@/super-action/button/ActionButton'
 import { RotateCw } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
 import { StatsDisplay } from './StatsDisplay'
 
 export const ReRollButton = ({ game }: { game: Game }) => {
@@ -11,35 +12,40 @@ export const ReRollButton = ({ game }: { game: Game }) => {
   const enoughGold = game.data.gold >= priceToReroll
   return (
     <>
-      <ActionButton
-        catchToast
-        hideIcon
-        variant={'outline'}
-        disabled={!enoughGold}
-        action={async () => {
-          'use server'
-          return gameAction({
-            gameId: game.id,
-            action: async ({ ctx }) => {
-              const { game } = ctx
-              game.data.shopRerolls += 1
-              if (game.data.gold < priceToReroll) {
-                throw new Error('not enough gold')
-              }
-              game.data.gold -= priceToReroll
-              game.data.shopItems = await generateShopItems({ game })
-            },
-          })
-        }}
-        className={cn(
-          'flex flex-row gap-2 items-center',
-          !enoughGold && 'grayscale',
-        )}
-      >
-        <RotateCw className="size-4" />
-        <div>Re-Roll Shop</div>
-        <StatsDisplay disableTooltip stats={{ gold: priceToReroll }} />
-      </ActionButton>
+      <Tooltip>
+        <TooltipTrigger>
+          <ActionButton
+            catchToast
+            hideIcon
+            variant={'outline'}
+            disabled={!enoughGold}
+            action={async () => {
+              'use server'
+              return gameAction({
+                gameId: game.id,
+                action: async ({ ctx }) => {
+                  const { game } = ctx
+                  game.data.shopRerolls += 1
+                  if (game.data.gold < priceToReroll) {
+                    throw new Error('not enough gold')
+                  }
+                  game.data.gold -= priceToReroll
+                  game.data.shopItems = await generateShopItems({ game })
+                },
+              })
+            }}
+            className={cn(
+              'flex flex-row gap-2 items-center',
+              !enoughGold && 'grayscale',
+            )}
+          >
+            <RotateCw className="size-4" />
+            {/* <div>Re-Roll Shop</div> */}
+            <StatsDisplay disableTooltip stats={{ gold: priceToReroll }} />
+          </ActionButton>
+        </TooltipTrigger>
+        <TooltipContent>Re-roll shop items</TooltipContent>
+      </Tooltip>
     </>
   )
 }
