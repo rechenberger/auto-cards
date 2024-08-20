88df475dc3e88614140156b8c0153443757b5427
Tristan Rechenberger <rechenberger@me.com>
2024-08-20 09:45:51 +0200

    💄 loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
+import { MainLogo } from '@/components/layout/MainLogo'
+import { LoaderCircle } from 'lucide-react'
 
 export default function Page() {
   return (
-    <div className="bg-background flex-1 w-full flex flex-col items-center justify-center">
-      <div className="mx-8 animate-pulse">
-        <Image src={'/favicon-white.svg'} width={384} height={384} alt="logo" />
+    <div className="bg-background flex-1 w-full flex flex-col items-center justify-center animate-pulse">
+      <div className="mx-8">
+        <MainLogo size="big" />
       </div>
+      <LoaderCircle className="text-primary size-6 animate-spin" />
     </div>
   )
 }
