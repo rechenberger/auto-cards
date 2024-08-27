0459d5e91158e96d3c79f52c834b90719a557642
Tristan Rechenberger <rechenberger@me.com>
2024-08-27 17:17:13 +0200

    📱

--- src/components/game/MatchReportDisplayToggle.tsx
+++ src/components/game/MatchReportDisplayToggle.tsx
@@ -19,7 +19,7 @@ export const MatchReportDisplayToggle = ({
           <Button variant="outline">Logs</Button>
         </CollapsibleTrigger>
         <CollapsibleContent>
-          <div className="max-h-96 overflow-auto rounded-lg self-stretch lg:self-center mt-2">
+          <div className="max-h-96 overflow-auto rounded-lg self-stretch lg:self-center mt-2 max-w-[calc(100vw-2rem)] xl:max-w-[560px]">
             <MatchReportDisplay matchReport={matchReport} />
           </div>
         </CollapsibleContent>
