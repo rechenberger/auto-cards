d285d46aa3636a65c27c540638b7272babb2bc8f
Tristan Rechenberger <rechenberger@me.com>
2024-08-27 21:23:26 +0200

    🥅 item page 404

--- src/app/(main)/items/[itemName]/page.tsx
+++ src/app/(main)/items/[itemName]/page.tsx
@@ -2,8 +2,10 @@ import { getIsAdmin } from '@/auth/getIsAdmin'
 import { AiImageGallery } from '@/components/ai/AiImageGallery'
 import { getItemAiImagePrompt } from '@/components/game/getItemAiImagePrompt'
 import { ItemCard } from '@/components/game/ItemCard'
+import { getItemByName } from '@/game/allItems'
 import { capitalCase } from 'change-case'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
 type PageProps = {
   params: {
@@ -19,6 +21,11 @@ export const generateMetadata = async ({
 
 export default async function Page({ params: { itemName } }: PageProps) {
   const isAdmin = await getIsAdmin({ allowDev: true })
+  try {
+    await getItemByName(itemName)
+  } catch (error) {
+    notFound()
+  }
   return (
     <>
       <div className="flex flex-row gap-4">
