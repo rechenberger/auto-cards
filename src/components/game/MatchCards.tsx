b12c8be0992c8fa2d4dd847fc97b1ed91c97f9e4
Tristan Rechenberger <rechenberger@me.com>
2024-08-27 08:45:57 +0200

    💄

--- src/components/game/MatchCards.tsx
+++ src/components/game/MatchCards.tsx
@@ -1,6 +1,7 @@
 import { Game, LoadoutData } from '@/db/schema-zod'
 import { countifyItems } from '@/game/countifyItems'
 import { orderItems } from '@/game/orderItems'
+import { cn } from '@/lib/utils'
 import { map } from 'lodash-es'
 import { Fragment } from 'react'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
@@ -20,32 +21,40 @@ export const MatchCards = async ({
 
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-1">
-        {map(items, (item) => (
-          <Fragment key={item.name}>
-            <Tooltip>
-              <TooltipTrigger>
-                <ItemCard
-                  game={game}
-                  name={item.name}
-                  count={item.count}
-                  size="80"
-                />
-              </TooltipTrigger>
-              <TooltipContent
-                className="p-0 border-none bg-transparent"
-                side={sideIdx === 0 ? 'right' : 'left'}
-              >
-                <ItemCard
-                  game={game}
-                  name={item.name}
-                  count={item.count}
-                  size="320"
-                />
-              </TooltipContent>
-            </Tooltip>
-          </Fragment>
-        ))}
+      <div className="grid grid-cols-2 md:grid-cols-4 grid-flow-dense">
+        {map(items, (item, itemIdx) => {
+          const isBig = itemIdx === 2 || itemIdx === 3
+          return (
+            <Fragment key={item.name}>
+              <Tooltip>
+                <TooltipTrigger
+                  className={cn(
+                    isBig && 'col-span-2 row-span-2',
+                    isBig ? 'm-2 scale-[105%]' : 'm-1',
+                  )}
+                >
+                  <ItemCard
+                    game={game}
+                    name={item.name}
+                    count={item.count}
+                    size={isBig ? '160' : '80'}
+                  />
+                </TooltipTrigger>
+                <TooltipContent
+                  className="p-0 border-none bg-transparent"
+                  side={sideIdx === 0 ? 'right' : 'left'}
+                >
+                  <ItemCard
+                    game={game}
+                    name={item.name}
+                    count={item.count}
+                    size="320"
+                  />
+                </TooltipContent>
+              </Tooltip>
+            </Fragment>
+          )
+        })}
       </div>
     </>
   )
