9ea9c2dada1690228afddb0cbc5bbee9176949fc
Tristan Rechenberger <rechenberger@me.com>
2024-08-28 20:03:54 +0200

    ✨ shop no duplicates

--- src/game/generateShopItems.ts
+++ src/game/generateShopItems.ts
@@ -2,13 +2,25 @@ import { Game, GameData } from '@/db/schema-zod'
 import { range } from 'lodash-es'
 import { getAllItems } from './allItems'
 import { NO_OF_SHOP_ITEMS, SALE_CHANCE } from './config'
-import { rngFloat, rngItem } from './seed'
+import { rngFloat, rngItems } from './seed'
 
 export const generateShopItems = async ({ game }: { game: Game }) => {
   const allItems = await getAllItems()
   const itemsForSale = allItems.filter((item) => !!item.price)
 
-  const shopSeed = [game.data.seed, game.data.roundNo, game.data.shopRerolls]
+  const shopSeed = [
+    game.data.seed,
+    game.data.roundNo,
+    game.data.shopRerolls,
+    'shop',
+  ]
+
+  // no duplicates
+  const items = rngItems({
+    seed: shopSeed,
+    items: itemsForSale,
+    count: NO_OF_SHOP_ITEMS,
+  })
 
   const shopItems: GameData['shopItems'] = range(NO_OF_SHOP_ITEMS).map(
     (idx) => {
@@ -17,12 +29,9 @@ export const generateShopItems = async ({ game }: { game: Game }) => {
         return oldItem
       }
 
-      const itemSeed = [...shopSeed, 'shopItem', idx]
+      const itemSeed = [...shopSeed, idx]
 
-      const item = rngItem({
-        seed: itemSeed,
-        items: itemsForSale,
-      })
+      const item = items[idx]
 
       const isOnSale =
         rngFloat({
