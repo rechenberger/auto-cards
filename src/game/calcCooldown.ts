953ba837cd8159cf9c288bd4a6cd12658ff7da9b
Tristan Rechenberger <rechenberger@me.com>
2024-08-28 12:07:46 +0200

    ♻️

--- src/game/calcCooldown.ts
+++ src/game/calcCooldown.ts
@@ -8,9 +8,15 @@ export const calcCooldown = ({
   cooldown: number
   stats: Stats
 }) => {
-  const slow = stats.slow ?? 0
-  const haste = stats.haste ?? 0
-  let result = cooldown * (1 + (slow - haste) / 100)
+  let multiplier = 1
+  if (stats.slow) {
+    multiplier += stats.slow / 100
+  }
+  if (stats.haste) {
+    multiplier -= stats.haste / 100
+  }
+
+  let result = cooldown * multiplier
   result = Math.ceil(result / COOLDOWN_PRECISION) * COOLDOWN_PRECISION
   result = Math.max(result, MIN_COOLDOWN)
   return result
