9dfd542a749dc86c7a0a3432d50f9f8aa3351374
Tristan Rechenberger <rechenberger@me.com>
2024-09-04 14:34:26 +0200

    ✨ match view themes

--- src/components/game/streamItemCard.tsx
+++ src/components/game/streamItemCard.tsx
@@ -25,7 +25,7 @@ export const streamItemCard = async ({
       description: (
         <>
           <div className="flex flex-col gap-4 max-h-[calc(100vh-80px)] overflow-auto">
-            <ItemCard name={name} size="320" />
+            <ItemCard name={name} size="320" themeId={themeId} />
             {changemaker && (
               <div className="bg-[#313130] text-white px-4 py-1 rounded-md">
                 Necessity: {Math.round(changemaker.necessity * 100)}%
