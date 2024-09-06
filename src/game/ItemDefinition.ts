42d933481469fd1cc3db47c27d3ade1226ef0d0d
Tristan Rechenberger <rechenberger@me.com>
2024-09-07 00:31:59 +0200

    ✨ modifiers

--- src/game/ItemDefinition.ts
+++ src/game/ItemDefinition.ts
@@ -3,6 +3,25 @@ import { Rarity } from './rarities'
 import { Stat, Stats } from './stats'
 import { Tag } from './tags'
 
+// Für jede Waffe oder Schild (bis zu 3) addiere 3 Damage
+// Für jedes Thorns addiere 1 Damage
+// Für jedes Food multipliziere die Damage mit 1.5
+export const Modifier = z.object({
+  arithmetic: z.enum(['multiply', 'add']),
+  targetStat: Stat,
+  targetStats: z.enum([
+    'statsSelf',
+    'statsEnemy',
+    'statsItem',
+    'statsRequired',
+    'attack',
+  ]),
+  sourceMultiplier: z.number(),
+  source: z.array(Tag.or(Stat)),
+  sourceCountMax: z.number().optional(),
+})
+export type Modifier = z.infer<typeof Modifier>
+
 const triggerEvents = [
   'onAttackBeforeHit',
   'onAttackAfterHit',
@@ -33,29 +52,7 @@ export const Trigger = z
     attack: Stats.optional(),
     statsItem: Stats.optional(),
     maxCount: z.number().optional(),
-
-    // Für jede Waffe oder Schild (bis zu 3) addiere 3 Damage
-    // Für jedes Thorns addiere 1 Damage
-    // Für jedes Food multipliziere die Damage mit 1.5
-
-    modifiers: z
-      .array(
-        z.object({
-          arithmetic: z.enum(['multiply', 'add']),
-          targetStat: Stat,
-          targetStats: z.enum([
-            'statsSelf',
-            'statsEnemy',
-            'statsItem',
-            'statsRequired',
-            'attack',
-          ]),
-          sourceMultiplier: z.number(),
-          source: z.array(Tag.or(Stat)),
-          sourceCountMax: z.number().optional(),
-        }),
-      )
-      .optional(),
+    modifiers: z.array(Modifier).optional(),
   })
   .and(TriggerWithCooldown.or(TriggerWithoutCooldown))
 export type Trigger = z.infer<typeof Trigger>
