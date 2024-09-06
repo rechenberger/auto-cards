dce181e8dca1ba40b9a5da8e8e481f7dc875e1bd
Tristan Rechenberger <rechenberger@me.com>
2024-09-07 00:44:33 +0200

    ✨ modifiers

--- src/game/ItemDefinition.ts
+++ src/game/ItemDefinition.ts
@@ -1,26 +1,8 @@
 import { z } from 'zod'
+import { Modifier } from './modifiers'
 import { Rarity } from './rarities'
-import { Stat, Stats } from './stats'
+import { Stats } from './stats'
 import { Tag } from './tags'
-
-// Für jede Waffe oder Schild (bis zu 3) addiere 3 Damage
-// Für jedes Thorns addiere 1 Damage
-// Für jedes Food multipliziere die Damage mit 1.5
-export const Modifier = z.object({
-  arithmetic: z.enum(['multiply', 'add']),
-  targetStat: Stat,
-  targetStats: z.enum([
-    'statsSelf',
-    'statsEnemy',
-    'statsItem',
-    'statsRequired',
-    'attack',
-  ]),
-  sourceMultiplier: z.number(),
-  source: z.array(Tag.or(Stat)),
-  sourceCountMax: z.number().optional(),
-})
-export type Modifier = z.infer<typeof Modifier>
 
 const triggerEvents = [
   'onAttackBeforeHit',
