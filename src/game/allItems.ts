8c08d9bc651fb641f400b33eb0ad7a1e274a8ff9
Tim Weiskopf <weiskopf@sodefa.de>
2024-09-06 02:18:55 +0200

    🍱 carrot

--- src/game/allItems.ts
+++ src/game/allItems.ts
@@ -711,6 +711,28 @@ const allItemsConst = [
       },
     ],
   },
+  {
+    name: 'carrot',
+    tags: ['food'],
+    rarity: 'common',
+    price: 3,
+    stats: {
+      space: space(-3),
+    },
+    triggers: [
+      {
+        type: 'interval',
+        cooldown: 3_000,
+        statsRequired: {
+          luck: 4,
+        },
+        statsSelf: {
+          empower: 1,
+        },
+        chancePercent: 50,
+      },
+    ],
+  },
 ] as const satisfies ItemDefinition[]
 
 export type ItemName = (typeof allItemsConst)[number]['name']
