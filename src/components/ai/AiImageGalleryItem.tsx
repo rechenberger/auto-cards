a9e187f7f80244f078d07b666b17c99df1fa3633
Tristan Rechenberger <rechenberger@me.com>
2024-09-08 22:31:48 +0200

    ✨ match bg

--- src/components/ai/AiImageGalleryItem.tsx
+++ src/components/ai/AiImageGalleryItem.tsx
@@ -1,11 +1,11 @@
-import { fallbackThemeId } from '@/game/themes'
+import { fallbackThemeId, ThemeId } from '@/game/themes'
 import { getItemAiImagePrompt } from '../game/getItemAiImagePrompt'
 import { AiImageGallery, AiImageGalleryProps } from './AiImageGallery'
 
 export const AiImageGalleryItem = async (
   props: Omit<AiImageGalleryProps, 'prompt'> & { itemId: string },
 ) => {
-  const themeId = await fallbackThemeId(props.themeId)
+  const themeId = await fallbackThemeId(props.themeId as ThemeId)
   const prompt = await getItemAiImagePrompt({
     name: props.itemId,
     themeId,
