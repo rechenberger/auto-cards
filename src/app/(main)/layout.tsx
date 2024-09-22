89b62460e73576cdbb4223a9da7e88d9072e71fc
Tristan Rechenberger <rechenberger@me.com>
2024-09-22 11:51:41 +0200

    📱 container px mobile

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <MainTop />
         <hr />
       </div>
-      <div className="container flex flex-col gap-8 py-8 flex-1">
+      <div className="container max-md:px-4 flex flex-col gap-8 py-8 flex-1">
         {children}
       </div>
     </>
