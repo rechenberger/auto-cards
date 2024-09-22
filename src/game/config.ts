58294565856213c28ed86c1ec94c24804372f963
Tristan Rechenberger <rechenberger@me.com>
2024-09-22 23:07:50 +0200

    🔧 NO_OF_LATEST_LOADOUTS 20

--- src/game/config.ts
+++ src/game/config.ts
@@ -8,7 +8,7 @@ export const FATIGUE_STARTS_AT = 15_000
 export const MIN_COOLDOWN = 100
 export const COOLDOWN_PRECISION = 100
 export const NO_OF_ROUNDS = 5
-export const NO_OF_LATEST_LOADOUTS = 5
+export const NO_OF_LATEST_LOADOUTS = 20
 export const LIMIT_GAME_OVERVIEW = 9
 
 export const IGNORE_SPACE = true
