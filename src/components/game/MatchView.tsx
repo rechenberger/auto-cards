b6422b747b9eb198289de78f2c08ff23ea4e9c38
Tristan Rechenberger <rechenberger@me.com>
2024-09-26 01:03:57 +0200

    ⚡️ 💄 MatchView grid

--- src/components/game/MatchView.tsx
+++ src/components/game/MatchView.tsx
@@ -2,6 +2,7 @@ import { Game, Match } from '@/db/schema-zod'
 import { generateChangemakers } from '@/game/generateChangemakers'
 import { generateMatch } from '@/game/generateMatch'
 import { fallbackThemeId } from '@/game/themes'
+import { cn } from '@/lib/utils'
 import { createId } from '@paralleldrive/cuid2'
 import { every } from 'lodash-es'
 import { AlertCircle, Swords } from 'lucide-react'
@@ -62,17 +63,50 @@ export const MatchView = async ({
   return (
     <>
       <MatchBackground themeIds={themeIds} autoGenerate={true} />
-      <div className="flex flex-col gap-4 flex-1">
-        <div className="flex flex-col xl:flex-row gap-2 flex-1 items-center">
-          <div className="hidden xl:flex">
-            <MatchCards
-              items={participants[0].loadout.data.items}
-              sideIdx={0}
-              changemakers={changemakers}
-              matchReport={matchReport}
-              themeId={themeIds[0]}
-            />
-          </div>
+      <div
+        className={cn(
+          'flex-1 grid',
+          'grid-cols-[1fr_1fr_!important]',
+          'grid-rows-[auto_auto_1fr_!important]',
+          "[grid-template:'middle_middle'_'stats_stats'_'left_right']",
+          'xl:grid-cols-[auto_1fr_auto_!important]',
+          'xl:grid-rows-[1fr_auto_auto_!important]',
+          "xl:[grid-template:'left_middle_right'_'stats_stats_stats']",
+        )}
+      >
+        <div
+          style={{
+            gridArea: 'left',
+          }}
+          className="flex flex-row gap-4 items-start xl:items-center justify-start"
+        >
+          <MatchCards
+            items={participants[0].loadout.data.items}
+            sideIdx={0}
+            changemakers={changemakers}
+            matchReport={matchReport}
+            themeId={themeIds[0]}
+          />
+        </div>
+        <div
+          style={{
+            gridArea: 'right',
+          }}
+          className="flex flex-row gap-4 items-start xl:items-center justify-end"
+        >
+          <MatchCards
+            items={participants[1].loadout.data.items}
+            sideIdx={1}
+            changemakers={changemakers}
+            matchReport={matchReport}
+            themeId={themeIds[1]}
+          />
+        </div>
+        <div
+          style={{
+            gridArea: 'middle',
+          }}
+        >
           <div className="flex-1 flex flex-col gap-2 items-center justify-center self-stretch">
             <MatchReportPlaybackControls matchReport={matchReport} />
             <MatchReportTabs
@@ -84,50 +118,28 @@ export const MatchView = async ({
             <div className="flex-1" />
             {!!game && <NextRoundButton game={game} />}
           </div>
-
-          <div className="hidden xl:flex">
-            <MatchCards
-              items={participants[1].loadout.data.items}
-              sideIdx={1}
-              changemakers={changemakers}
-              matchReport={matchReport}
-              themeId={themeIds[1]}
-            />
-          </div>
         </div>
-        <div className="flex flex-row gap-2 justify-center relative">
-          <div className="max-xl:flex-1 flex flex-col gap-4">
-            <MatchSide
-              sideIdx={0}
-              participant={participants[0]}
-              matchReport={matchReport}
-            />
-            <div className="xl:hidden self-start">
-              <MatchCards
-                items={participants[0].loadout.data.items}
+        <div
+          style={{
+            gridArea: 'stats',
+          }}
+        >
+          <div className="flex flex-row gap-2 justify-center relative">
+            <div className="max-xl:flex-1 flex flex-col gap-4">
+              <MatchSide
                 sideIdx={0}
-                changemakers={changemakers}
+                participant={participants[0]}
                 matchReport={matchReport}
-                themeId={themeIds[0]}
               />
             </div>
-          </div>
-          <div className="absolute top-8 z-10">
-            <Swords className="size-8 xl:size-16" />
-          </div>
-          <div className="max-xl:flex-1 flex flex-col gap-4">
-            <MatchSide
-              sideIdx={1}
-              participant={participants[1]}
-              matchReport={matchReport}
-            />
-            <div className="xl:hidden self-end">
-              <MatchCards
-                items={participants[1].loadout.data.items}
+            <div className="absolute top-8 z-10">
+              <Swords className="size-8 xl:size-16" />
+            </div>
+            <div className="max-xl:flex-1 flex flex-col gap-4">
+              <MatchSide
                 sideIdx={1}
-                changemakers={changemakers}
+                participant={participants[1]}
                 matchReport={matchReport}
-                themeId={themeIds[1]}
               />
             </div>
           </div>
