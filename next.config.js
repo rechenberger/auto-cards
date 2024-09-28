05c18054f2faae23b47214506becc37bf376b161
Tristan Rechenberger <rechenberger@me.com>
2024-09-28 10:03:02 +0200

    🔧 redirect to new host

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // Match all routes
+        has: [
+          {
+            type: 'host',
+            value: 'battlegrounds.sackofsecrets.com', // The domain you want to redirect from
+          },
+        ],
+        destination: 'https://auto-cards.com/:path*', // The domain you want to redirect to
+        permanent: true, // Use 308 for permanent redirect
+      },
+    ]
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.output.publicPath = ''
