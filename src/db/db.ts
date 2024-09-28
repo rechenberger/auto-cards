fe0807e66a400fd17fbbf1cb7097c9b107edd32e
Tristan Rechenberger <rechenberger@me.com>
2024-09-28 09:37:56 +0200

    ⚡️ generateMatch on client

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,10 @@
-import { Client, createClient } from '@libsql/client'
+import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from './schema'
 
-const client: Client =
-  typeof window === 'undefined'
-    ? createClient({
-        url: process.env.DB_URL!,
-        authToken: process.env.DB_TOKEN!,
-      })
-    : (null as any)
+const client = createClient({
+  url: process.env.DB_URL!,
+  authToken: process.env.DB_TOKEN!,
+})
 
 export const db = drizzle(client, { schema })
