b27d79002b316cb19dbe5c265db1f9dd2def7c8b
Tristan Rechenberger <rechenberger@me.com>
2024-09-29 09:08:35 +0200

    ✨ best items page

--- src/app/(main)/watch/items/page.tsx
+++ src/app/(main)/watch/items/page.tsx
@@ -1,4 +1,5 @@
 import { getAllItems } from '@/game/allItems'
+import { LEADERBOARD_LIMIT } from '@/game/config'
 import { getLeaderboardRanked } from '@/game/getLeaderboard'
 import { capitalCase } from 'change-case'
 import { orderBy, sumBy } from 'lodash-es'
@@ -26,11 +27,9 @@ export default async function Page() {
         count,
       }
     })
-    const totalCount = sumBy(entriesWithCount, (e) => e.count)
     return {
       item,
       entriesWithCount,
-      totalCount,
     }
   })
 
@@ -38,12 +37,13 @@ export default async function Page() {
     <>
       <div className="grid xl:grid-cols-2 gap-4">
         <ItemChart
-          title="Leaderboard Items"
-          valueLabel="Total Count"
+          title="Total Count"
+          subTitle="count in leaderboard"
+          valueLabel="count"
           data={orderBy(
             itemsRanked.map((item) => ({
               name: capitalCase(item.item.name),
-              value: item.totalCount,
+              value: sumBy(item.entriesWithCount, (e) => e.count),
               fill: 'hsl(var(--chart-1))',
             })),
             (i) => i.value,
@@ -51,12 +51,46 @@ export default async function Page() {
           )}
         />
         <ItemChart
-          title="Leaderboard Items"
-          valueLabel="Build Count"
+          title="Build Count"
+          subTitle="count builds that have this item"
+          valueLabel="builds"
           data={orderBy(
             itemsRanked.map((item) => ({
               name: capitalCase(item.item.name),
-              value: item.entriesWithCount.filter((e) => e.count > 0).length,
+              value: sumBy(item.entriesWithCount, (e) => (e.count ? 1 : 0)),
+              fill: 'hsl(var(--chart-1))',
+            })),
+            (i) => i.value,
+            'desc',
+          )}
+        />
+        <ItemChart
+          title="Total Count (ranked)"
+          subTitle={`count in leaderboard and multiply by ${LEADERBOARD_LIMIT} - rank`}
+          valueLabel="points"
+          data={orderBy(
+            itemsRanked.map((item) => ({
+              name: capitalCase(item.item.name),
+              value: sumBy(
+                item.entriesWithCount,
+                (e) => e.count * (LEADERBOARD_LIMIT - e.rank),
+              ),
+              fill: 'hsl(var(--chart-1))',
+            })),
+            (i) => i.value,
+            'desc',
+          )}
+        />
+        <ItemChart
+          title="Build Count (ranked)"
+          subTitle={`count builds that have this item and multiply by ${LEADERBOARD_LIMIT} - rank`}
+          valueLabel="points"
+          data={orderBy(
+            itemsRanked.map((item) => ({
+              name: capitalCase(item.item.name),
+              value: sumBy(item.entriesWithCount, (e) =>
+                e.count ? LEADERBOARD_LIMIT - e.rank : 0,
+              ),
               fill: 'hsl(var(--chart-1))',
             })),
             (i) => i.value,
