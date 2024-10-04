cdd7698e32a26fa1b86a49b75b46006adb650e57
Tristan Rechenberger <rechenberger@me.com>
2024-10-04 17:46:31 +0200

    🗃️ leaderboardEntry gameId

--- src/app/(main)/admin/migrations/page.tsx
+++ src/app/(main)/admin/migrations/page.tsx
@@ -14,32 +14,34 @@ export default async function Page() {
   await throwIfNotAdmin({ allowDev: true })
   return (
     <>
-      <ActionButton
-        action={async () => {
-          'use server'
-          const leaderboard = await db.query.leaderboardEntry.findMany({
-            where: isNull(leaderboardEntry.gameId),
-            with: {
-              loadout: true,
-            },
-            // limit: 10,
-          })
-          for (const entries of chunk(leaderboard, 10)) {
-            await Promise.all(
-              entries.map(async (entry) => {
-                await db
-                  .update(leaderboardEntry)
-                  .set({
-                    gameId: entry.loadout.gameId,
-                  })
-                  .where(eq(leaderboardEntry.id, entry.id))
-              }),
-            )
-          }
-        }}
-      >
-        Leaderboard GameId
-      </ActionButton>
+      <div className="flex flex-col gap-4 self-center">
+        <ActionButton
+          action={async () => {
+            'use server'
+            const leaderboard = await db.query.leaderboardEntry.findMany({
+              where: isNull(leaderboardEntry.gameId),
+              with: {
+                loadout: true,
+              },
+              // limit: 10,
+            })
+            for (const entries of chunk(leaderboard, 10)) {
+              await Promise.all(
+                entries.map(async (entry) => {
+                  await db
+                    .update(leaderboardEntry)
+                    .set({
+                      gameId: entry.loadout.gameId,
+                    })
+                    .where(eq(leaderboardEntry.id, entry.id))
+                }),
+              )
+            }
+          }}
+        >
+          Leaderboard GameId
+        </ActionButton>
+      </div>
     </>
   )
 }
