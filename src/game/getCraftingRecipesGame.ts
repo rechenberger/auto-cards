57e4f58ae6c5bcd9505a3fdd66d71afcd78c9e8f
Tristan Rechenberger <rechenberger@me.com>
2024-10-09 14:03:12 +0200

    ✨ uniqueAlreadyCrafted

--- src/game/getCraftingRecipesGame.ts
+++ src/game/getCraftingRecipesGame.ts
@@ -1,10 +1,12 @@
 import { Game } from '@/db/schema-zod'
 import { every, filter, map, orderBy, some } from 'lodash-es'
+import { getAllItems } from './allItems'
 import { countifyItems } from './countifyItems'
 import { getCraftingRecipes } from './craftingRecipes'
 
 export const getCraftingRecipesGame = async ({ game }: { game?: Game }) => {
   const all = await getCraftingRecipes()
+  const allItems = await getAllItems()
   const myItems = countifyItems(game?.data.currentLoadout.items ?? [])
 
   let results = map(all, (recipe) => {
@@ -23,16 +25,34 @@ export const getCraftingRecipesGame = async ({ game }: { game?: Game }) => {
     const hasAll = every(input, (i) => i.hasEnough)
     const hasSome = some(input, (i) => i.countCurrent > 0)
 
+    const outputs = map(recipe.output, (item) => {
+      const def = allItems.find((i) => i.name === item.name)
+      if (!def) {
+        throw new Error(`Item ${item.name} not found`)
+      }
+      const myItem = myItems.find((i) => i.name === item.name)
+      const countCurrent = myItem?.count ?? 0
+      let uniqueAlreadyCrafted = def.unique && countCurrent > 0
+      return {
+        ...item,
+        uniqueAlreadyCrafted,
+      }
+    })
+
+    const uniqueAlreadyCrafted = some(outputs, (i) => i.uniqueAlreadyCrafted)
+
     return {
       ...recipe,
       input,
       hasAll,
       hasSome,
+      uniqueAlreadyCrafted,
     }
   })
 
   if (game) {
     results = filter(results, (r) => r.hasSome)
+    results = filter(results, (r) => !r.uniqueAlreadyCrafted)
     results = orderBy(results, (r) => r.hasAll, 'desc')
   }
 
