29011c23e9ad8ea78d8960baca1651d6c7651c1c
Tristan Rechenberger <rechenberger@me.com>
2024-10-12 10:13:41 +0200

    ✨ SimpleDataTable

--- src/app/(main)/dev/rng/page.tsx
+++ src/app/(main)/dev/rng/page.tsx
@@ -2,13 +2,22 @@ import { isDevDb } from '@/auth/dev'
 import { notFoundIfNotAdmin } from '@/auth/getIsAdmin'
 import { getMyUserIdOrThrow } from '@/auth/getMyUser'
 import { SimpleDataCard } from '@/components/simple/SimpleDataCard'
+import { SimpleDataTableCard } from '@/components/simple/SimpleDataTable'
 import { getItemByName } from '@/game/allItems'
+import { NO_OF_SHOP_ITEMS } from '@/game/config'
 import { createGame } from '@/game/createGame'
-import { countBy, map, orderBy, range } from 'lodash-es'
-import { Fragment } from 'react'
+import { capitalCase } from 'change-case'
+import { countBy, groupBy, map, orderBy, range, sumBy } from 'lodash-es'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'RNG',
+}
 
 export default async function Page() {
-  const NO_OF_GAMES = 100
+  const NO_OF_GAMES = 10_000
+  const NO_OF_ITEMS = NO_OF_GAMES * NO_OF_SHOP_ITEMS
+
   await notFoundIfNotAdmin({ allowDev: true })
 
   const userId = await getMyUserIdOrThrow()
@@ -28,18 +37,33 @@ export default async function Page() {
         games.flatMap((game) => game.data.shopItems),
         (i) => i.name,
       ),
-      (count, name) => ({ count, name }),
+      (count, name) => ({
+        count,
+        name,
+        countRelative: `${((100 * count) / NO_OF_ITEMS).toFixed(2)}%`,
+      }),
     ),
     'count',
     'desc',
   )
 
-  const withItemDefs = await Promise.all(
-    counts.map(async ({ name, count }) => {
+  const byItems = await Promise.all(
+    counts.map(async ({ name, count, countRelative }) => {
       const def = await getItemByName(name)
-      return { name, count, rarity: def.rarity }
+      return { name, count, countRelative, rarity: def.rarity }
     }),
   )
+
+  const byRarity = map(groupBy(byItems, 'rarity'), (items, rarity) => {
+    const count = sumBy(items, (i) => i.count)
+    const countRelative = `${((100 * count) / NO_OF_ITEMS).toFixed(2)}%`
+
+    return {
+      rarity,
+      count,
+      countRelative,
+    }
+  })
 
   // const numbers = range(10000).map((idx) =>
   //   rngInt({
@@ -56,13 +80,29 @@ export default async function Page() {
   return (
     <>
       <h1>Dev</h1>
-      <div className="grid grid-cols-4 gap-2">
+      {/* <div className="grid grid-cols-4 gap-2">
         {withItemDefs.map((item) => (
           <Fragment key={item.name}>
             <SimpleDataCard data={item} />
           </Fragment>
         ))}
-      </div>
+      </div> */}
+      <SimpleDataCard
+        data={{
+          NO_OF_GAMES,
+          NO_OF_ITEMS,
+        }}
+      />
+      <SimpleDataTableCard
+        title="Rarity"
+        data={byRarity}
+        formatKey={capitalCase}
+      />
+      <SimpleDataTableCard
+        title="Shop Items"
+        data={byItems}
+        formatKey={capitalCase}
+      />
       {/* <h1>Mean: {mean(numbers)}</h1>
       <div className="grid grid-cols-4 gap-2">
         {numbers.map((n, idx) => (
