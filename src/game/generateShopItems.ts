0251e03fe0da10db43fb49597542f2781abae445
Tristan Rechenberger <rechenberger@me.com>
2024-10-12 09:39:12 +0200

    ♻️ generateShopItems weights

--- src/game/generateShopItems.ts
+++ src/game/generateShopItems.ts
@@ -1,11 +1,8 @@
 import { Game, GameData } from '@/db/schema-zod'
-import { forEach } from 'lodash-es'
 import { getAllItems } from './allItems'
 import { NO_OF_SHOP_ITEMS, SALE_CHANCE } from './config'
-import { ItemDefinition } from './ItemDefinition'
-import { rarityCountsByWeights } from './rarityCountsByWeights'
 import { roundStats } from './roundStats'
-import { rngFloat, rngItems } from './seed'
+import { rngFloat, rngItemsWithWeights } from './seed'
 
 export const generateShopItems = async ({
   game,
@@ -41,33 +38,33 @@ export const generateShopItems = async ({
   const oldItems = game.data.shopItems.filter((item) => item.isReserved)
   const itemsToGenerate = NO_OF_SHOP_ITEMS - oldItems.length
 
-  let items: ItemDefinition[] = []
   const roundStat = roundStats[game.data.roundNo]
-  if (skipRarityWeights || !roundStat.rarityWeights) {
-    items = rngItems({
-      seed: shopSeed,
-      items: itemsForSale,
-      count: itemsToGenerate,
-    })
-  } else {
-    const counts = rarityCountsByWeights({
-      rarityWeights: roundStat.rarityWeights,
-      seed: shopSeed,
-      count: itemsToGenerate,
-    })
-    forEach(counts, (count, rarity) => {
-      if (!count) return
-      items.push(
-        ...rngItems({
-          seed: [...shopSeed, rarity],
-          items: itemsForSale.filter((item) => item.rarity === rarity),
-          count,
-        }).filter(Boolean),
-      )
-    })
-  }
+  const itemsWeighted = itemsForSale
+    .map((item) => {
+      let weight = 1
 
-  const shopItems: GameData['shopItems'] = items.map((newItem, idx) => {
+      if (!skipRarityWeights && roundStat.rarityWeights) {
+        if (!item.rarity) {
+          throw new Error(`Item ${item.name} has no rarity`)
+        }
+        const rarityWeight = roundStat.rarityWeights[item.rarity]
+        weight *= rarityWeight ?? 0
+      }
+
+      return {
+        item,
+        weight,
+      }
+    })
+    .filter((i) => i.weight > 0)
+
+  const newItems = rngItemsWithWeights({
+    seed: shopSeed,
+    items: itemsWeighted,
+    count: itemsToGenerate,
+  })
+
+  const shopItems: GameData['shopItems'] = newItems.map((newItem, idx) => {
     const itemSeed = [...shopSeed, idx]
 
     const isOnSale =
