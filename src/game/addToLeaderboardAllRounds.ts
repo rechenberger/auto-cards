f3b2c13c14f3c2fcc46dbebe394fb0ddd67a3f0e
Tristan Rechenberger <rechenberger@me.com>
2024-10-15 23:56:41 +0200

    ✨ addToLeaderboardAllRounds

--- src/game/addToLeaderboardAllRounds.ts
+++ src/game/addToLeaderboardAllRounds.ts
@@ -1,6 +1,7 @@
 import { db } from '@/db/db'
 import { schema } from '@/db/schema-export'
 import { eq } from 'drizzle-orm'
+import { orderBy } from 'lodash-es'
 import { addToLeaderboard } from './addToLeaderboard'
 
 export const addToLeaderboardAllRounds = async ({
@@ -8,9 +9,11 @@ export const addToLeaderboardAllRounds = async ({
 }: {
   gameId: string
 }) => {
-  const loadouts = await db.query.loadout.findMany({
+  let loadouts = await db.query.loadout.findMany({
     where: eq(schema.loadout.gameId, gameId),
   })
+
+  loadouts = orderBy(loadouts, (l) => l.roundNo, 'asc')
 
   for (const loadout of loadouts) {
     await addToLeaderboard({ loadout })
