70438ceaea19f2205889680ad0d987fbd8cafb47
Tristan Rechenberger <rechenberger@me.com>
2024-10-27 10:02:23 +0100

    ✨ itemization

--- src/components/game/LoadoutDisplay.tsx
+++ src/components/game/LoadoutDisplay.tsx
@@ -28,8 +28,8 @@ export const LoadoutDisplay = async ({
     <>
       <div className="xl:hidden">
         <CardRow>
-          {map(items, (item) => (
-            <Fragment key={item.name}>
+          {map(items, (item, idx) => (
+            <Fragment key={idx}>
               <ItemCard
                 game={game}
                 name={item.name}
