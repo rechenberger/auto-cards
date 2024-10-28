54e014c46c5d16dcd8f28e289bf3978802936e0a
Tristan Rechenberger <rechenberger@me.com>
2024-10-28 13:21:07 +0100

    ✨ collector

--- src/components/game/collector/CollectorAdminButtons.tsx
+++ src/components/game/collector/CollectorAdminButtons.tsx
@@ -1,22 +1,45 @@
 import { Game } from '@/db/schema-zod'
 import { gameAction } from '@/game/gameAction'
+import { generateShopItems } from '@/game/generateShopItems'
 import { ActionButton } from '@/super-action/button/ActionButton'
-import { Plus } from 'lucide-react'
+import { Plus, Trash } from 'lucide-react'
 
 export const CollectorAdminButtons = ({ game }: { game: Game }) => {
   return (
     <>
-      <ActionButton
-        icon={<Plus />}
-        variant="outline"
-        action={async () => {
-          'use server'
-          return gameAction({
-            gameId: game.id,
-            action: async ({ ctx }) => {},
-          })
-        }}
-      />
+      <div className="flex flex-row gap-2">
+        <ActionButton
+          icon={<Plus />}
+          variant="outline"
+          action={async () => {
+            'use server'
+            return gameAction({
+              gameId: game.id,
+              action: async ({ ctx }) => {
+                const shopItems = await generateShopItems({ game })
+                ctx.game.data.currentLoadout.items.push(...shopItems)
+              },
+            })
+          }}
+        />
+        <ActionButton
+          icon={<Trash />}
+          variant="outline"
+          action={async () => {
+            'use server'
+            return gameAction({
+              gameId: game.id,
+              action: async ({ ctx }) => {
+                ctx.game.data.currentLoadout.items = [
+                  {
+                    name: 'hero',
+                  },
+                ]
+              },
+            })
+          }}
+        />
+      </div>
     </>
   )
 }
