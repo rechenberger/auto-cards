18712cdfc2d3fb400c5d503cdc98e0b7076aa86e
Tristan Rechenberger <rechenberger@me.com>
2024-10-29 12:24:02 +0100

    ✨ checkCollectorLoadout

--- src/components/game/collector/CollectorLoadoutCheck.tsx
+++ src/components/game/collector/CollectorLoadoutCheck.tsx
@@ -10,35 +10,47 @@ export const CollectorLoadoutCheck = async ({ game }: { game: Game }) => {
   const check = await checkCollectorLoadout({ game })
   return (
     <>
-      <Card
-        className={cn(
-          'p-2 flex flex-col gap-1',
-          check.allGood ? 'bg-green-500' : 'bg-red-500',
-        )}
-      >
-        <div className="flex flex-row gap-2 items-center">
-          {check.allGood ? (
-            <CheckCircle className="size-4" />
-          ) : (
-            <XCircle className="size-4" />
+      <div className="flex flex-col gap-2">
+        <Card
+          className={cn(
+            'p-2 flex flex-col gap-1',
+            check.priceInBudget ? 'text-green-500' : 'bg-red-500',
           )}
-          <div className="flex-1">
-            {check.allGood ? 'Ready to fight' : 'Overloaded'}
+        >
+          <div className="flex flex-row gap-2 text-sm">
+            {check.priceInBudget ? (
+              <CheckCircle className="size-4" />
+            ) : (
+              <XCircle className="size-4" />
+            )}
+            <div className="flex-1">Space</div>
+            <div>
+              {check.priceLimit - check.priceCurrent}/{check.priceLimit}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col text-sm">
-          {check.countTooMany.map((i) => (
-            <Fragment key={i.name}>
-              <div className="flex flex-row gap-2">
-                <div className="flex-1">- too many {capitalCase(i.name)}</div>
-                <div>
-                  ({i.count}/{i.countMax})
-                </div>
-              </div>
-            </Fragment>
-          ))}
-        </div>
-      </Card>
+        </Card>
+        {!check.countInBudget && (
+          <Card
+            className={cn(
+              'p-2 flex flex-col gap-1',
+              check.countInBudget ? 'text-green-500' : 'bg-red-500',
+            )}
+          >
+            <div className="flex flex-col text-sm">
+              {check.countTooMany.map((i) => (
+                <Fragment key={i.name}>
+                  <div className="flex flex-row gap-2">
+                    <div className="flex-1">Too many {capitalCase(i.name)}</div>
+                    <div>
+                      ({i.count}/{i.countMax})
+                    </div>
+                  </div>
+                </Fragment>
+              ))}
+            </div>
+          </Card>
+        )}
+      </div>
     </>
   )
 }
