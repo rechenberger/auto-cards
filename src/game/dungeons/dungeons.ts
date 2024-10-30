c65acf45e9d94ce1d36011ec67d9a5b28f8191ef
Tristan Rechenberger <rechenberger@me.com>
2024-10-30 10:11:00 +0100

    🚚 dungeons

--- src/game/dungeons/dungeons.ts
+++ src/game/dungeons/dungeons.ts
@@ -1,10 +1,10 @@
 import { ItemData } from '@/components/game/ItemData'
-import { generateCollectorItem } from '@/components/game/collector/generateCollectorItem'
 import { Game } from '@/db/schema-zod'
 import { z } from 'zod'
 import { LoadoutData } from '../LoadoutData'
 import { ItemName } from '../allItems'
 import { SeedArray, rngFloat } from '../seed'
+import { trainingGrounds } from './trainingGrounds'
 
 export const DungeonRoom = z.discriminatedUnion('type', [
   z.object({
@@ -16,7 +16,7 @@ export const DungeonRoom = z.discriminatedUnion('type', [
     items: z.array(ItemData),
   }),
 ])
-type DungeonRoom = z.infer<typeof DungeonRoom>
+export type DungeonRoom = z.infer<typeof DungeonRoom>
 
 const allDungeonNames = ['trainingGrounds', 'adventureTrail'] as const
 export const DungeonName = z.enum(allDungeonNames)
@@ -69,64 +69,7 @@ const simpleRoomsToRooms = ({
 }
 
 const allDungeonsRaw: DungeonDefinition[] = [
-  {
-    name: 'trainingGrounds',
-    description:
-      'A little training never hurt nobody. Play through this once and get some basic equipment.',
-    levelMax: 5,
-    levelOnlyOnce: true,
-    generate: async ({ game, seed, level }) => {
-      const loadout: LoadoutData = {
-        items: [{ name: 'hero' }],
-      }
-      if (level >= 2) {
-        loadout.items.push({
-          name: 'woodenSword',
-          rarity: 'common',
-          aspects: [],
-        })
-      }
-      if (level >= 3) {
-        loadout.items.push({
-          name: 'woodenBuckler',
-          rarity: 'common',
-          aspects: [],
-        })
-      }
-      if (level >= 4) {
-        loadout.items.push({
-          name: 'leatherArmor',
-          rarity: 'common',
-          aspects: [],
-        })
-      }
-      if (level >= 5) {
-        loadout.items.push(
-          { name: 'beer', rarity: 'common', aspects: [] },
-          { name: 'beerFest', rarity: 'common', aspects: [] },
-        )
-      }
-
-      const reward = await generateCollectorItem({
-        game,
-        seed,
-        rarity: level === 5 ? 'uncommon' : 'common',
-      })
-      const rooms: DungeonRoom[] = [
-        {
-          type: 'fight',
-          loadout,
-        },
-        {
-          type: 'reward',
-          items: [reward],
-        },
-      ]
-      return {
-        rooms,
-      }
-    },
-  },
+  trainingGrounds,
   {
     name: 'adventureTrail',
     description:
---
+++ src/game/dungeons/trainingGrounds.ts
@@ -0,0 +1,62 @@
+import { generateCollectorItem } from '@/components/game/collector/generateCollectorItem'
+import { LoadoutData } from '../LoadoutData'
+import { DungeonDefinition, DungeonRoom } from './dungeons'
+
+export const trainingGrounds: DungeonDefinition = {
+  name: 'trainingGrounds',
+  description:
+    'A little training never hurt nobody. Play through this once and get some basic equipment.',
+  levelMax: 5,
+  levelOnlyOnce: true,
+  generate: async ({ game, seed, level }) => {
+    const loadout: LoadoutData = {
+      items: [{ name: 'hero' }],
+    }
+    if (level >= 2) {
+      loadout.items.push({
+        name: 'woodenSword',
+        rarity: 'common',
+        aspects: [],
+      })
+    }
+    if (level >= 3) {
+      loadout.items.push({
+        name: 'woodenBuckler',
+        rarity: 'common',
+        aspects: [],
+      })
+    }
+    if (level >= 4) {
+      loadout.items.push({
+        name: 'leatherArmor',
+        rarity: 'common',
+        aspects: [],
+      })
+    }
+    if (level >= 5) {
+      loadout.items.push(
+        { name: 'beer', rarity: 'common', aspects: [] },
+        { name: 'beerFest', rarity: 'common', aspects: [] },
+      )
+    }
+
+    const reward = await generateCollectorItem({
+      game,
+      seed,
+      rarity: level === 5 ? 'uncommon' : 'common',
+    })
+    const rooms: DungeonRoom[] = [
+      {
+        type: 'fight',
+        loadout,
+      },
+      {
+        type: 'reward',
+        items: [reward],
+      },
+    ]
+    return {
+      rooms,
+    }
+  },
+}
