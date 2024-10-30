f87556e6af2f53444e44e9cc6290a8c664299f0a
Tristan Rechenberger <rechenberger@me.com>
2024-10-30 10:22:10 +0100

    ✨ dungeons

--- src/game/dungeons/adventureTrail.ts
+++ src/game/dungeons/adventureTrail.ts
@@ -1,57 +1,35 @@
 import { ItemData } from '@/components/game/ItemData'
-import { ItemName } from '../allItems'
-import { SeedArray, rngFloat } from '../seed'
+import { rngFloat, rngGenerator } from '../seed'
 import { DungeonDefinition, DungeonRoom } from './DungeonDefinition'
-
-const simpleRoomsToRooms = ({
-  simpleRooms,
-  seed,
-  level,
-}: {
-  simpleRooms: { monsters: ItemName[] }[]
-  seed: SeedArray
-  level: number
-}) => {
-  const rooms: DungeonRoom[] = simpleRooms.map((simpleRoom, roomIdx) => {
-    const monsters: ItemData[] = simpleRoom.monsters.map(
-      (monster, monsterIdx) => ({
-        name: monster,
-        aspects: [
-          {
-            name: 'monsterPower',
-            rnd: rngFloat({
-              seed: [...seed, 'room', roomIdx, 'monster', monsterIdx],
-            }),
-            multiplier: 1.2 ** level,
-          },
-        ],
-      }),
-    )
-
-    return {
-      type: 'fight',
-      loadout: {
-        items: [{ name: 'hero' }, ...monsters],
-      },
-    }
-  })
-
-  return rooms
-}
 
 export const adventureTrail: DungeonDefinition = {
   name: 'adventureTrail',
   description:
     'An infinitely repeatable trail of adventure that leads to the greatest of treasures.',
   levelMax: 100,
-  generate: async ({ seed, level }) => {
-    const simpleRooms: { monsters: ItemName[] }[] = [
-      { monsters: ['scarecrow'] },
-      { monsters: ['wilma'] },
-      { monsters: ['scarecrow', 'wilma'] },
-    ]
+  generate: async ({ seed: _seed, level }) => {
+    const seed = rngGenerator({ seed: _seed })
+
+    const giveMonsterPower = (monster: ItemData) => {
+      const rnd = rngFloat({
+        seed,
+      })
+      return {
+        ...monster,
+        aspects: [
+          ...(monster.aspects ?? []),
+          { name: 'monsterPower', rnd, multiplier: 1.2 ** level },
+        ],
+      } satisfies ItemData
+    }
+
     const rooms: DungeonRoom[] = [
-      ...simpleRoomsToRooms({ simpleRooms, seed, level }),
+      {
+        type: 'fight',
+        loadout: {
+          items: [{ name: 'hero' }, giveMonsterPower({ name: 'scarecrow' })],
+        },
+      },
       {
         type: 'reward',
         items: [{ name: 'banana', aspects: [], rarity: 'common' }],
